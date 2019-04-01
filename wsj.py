23c0118c2dfb7eaee1378d1b5b39712417d916da
makoa <santarinimakoa@gmail.com>
2019-03-31 23:15:39 -1000

    Launches VPN. Locates start button.

--- wsj.py
+++ wsj.py
@@ -1,1 +1,7 @@
+import subprocess
+import time
 import pyautogui
+subprocess.Popen(r"C:\Program Files\Trust.Zone VPN Client\trustzone.exe", close_fds=True)
+time.sleep(5)
+startButton = pyautogui.locateOnScreen('trustZoneStartButton.png', grayscale=True, confidence=.5)
+print(startButton)
