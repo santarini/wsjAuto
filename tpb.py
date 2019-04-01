90286f4edc8150eda24193d4fda029740e921406
makoa <santarinimakoa@gmail.com>
2019-04-01 01:12:21 -1000

    Can't figure out how to get the tbp table >8x

--- tpb.py
+++ tpb.py
@@ -2,6 +2,13 @@ import requests
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import bs4 as bs
+import datetime
+
+d = datetime.datetime.today()
+print(d.strftime('%B %d, %Y'))
+dateString = d.strftime('%B %d, %Y')
+
+
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument("--incognito")
@@ -10,6 +17,32 @@ driver = webdriver.Chrome(r"C:\Users\CommandCenter\AppData\Local\Programs\Python
 #driver = webdriver.Chrome(r"C:\Program Files\Python\Python36\chromedriver.exe")
 
 driver.get('https://thepiratebay.org/user/surferbroadband/')
+response = driver.page_source
+#searchTable = driver.find_element_by_id('searchResult')
+
+soup = bs.BeautifulSoup(response, 'lxml')
+searchTable = soup.find("table", {"id": "searchResult"})
+searchTableBody = searchTable.find('tbody')
+for tr in searchTableBody.findAll('tr'):
+    for td in tr.findAll('td')[1]:
+        print(td)
+
+        print('#$#########################')
+##    for td in tr.findAll('td')[1]:
+##        for div in tr.findAll('div'):
+##            linkRef = div.find('a')
+##        if dateString in linkRef.text:
+##            print("OH SHIT!")
+        #print(td)
+        #for a in td.findAll('a')[0]:
+        #        print(a)
+        #print(td)
+    #for second td in each tr
+    #for first div in td
+    #for first a in div
+    #if title contains today's date
+    #go to link in next a
+    
 
 #find the table
 #if the link in the table contains today's date
